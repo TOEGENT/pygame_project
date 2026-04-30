388ec055d9b8e2da53cc73917c51dc4a89e078e0
KOTO <olegkadushkin555@gmail.com>
2026-04-30 08:52:55 +0500

    Добавил трения и убрал баги с замедлением и неверным управлением

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("My Game")
 
-SPEED = 1
+SPEED = 0.001
+FRICTION = 0.0005
 
 class State:
     COLLISION = "Collision"
@@ -43,10 +44,11 @@ class GameState:
         
     
 
-
-    def resolve_collision(self,other):
+    @staticmethod
+    def resolve_collision(ball, other):
         # Вычисляем нормальный вектор
-        normal = [other.pos[0] - self.player.pos[0], other.pos[1] - self.player.pos[1]]
+
+        normal = [other.pos[0] - ball.pos[0], other.pos[1] - ball   .pos[1]]
         distance = math.sqrt(normal[0]**2 + normal[1]**2)
         if distance == 0:
             return  # Избегаем деления на ноль
@@ -54,8 +56,8 @@ class GameState:
         normal[1] /= distance
 
         # Вычисляем относительную скорость
-        relative_velocity = [self.player.velocity[0] - other.velocity[0],
-                             self.player.velocity[1] - other.velocity[1]]
+        relative_velocity = [ball.velocity[0] - other.velocity[0],
+                             ball.velocity[1] - other.velocity[1]]
 
         # Вычисляем скорость по нормали
         velocity_along_normal = relative_velocity[0] * normal[0] + relative_velocity[1] * normal[1]
@@ -68,58 +70,80 @@ class GameState:
 
         # Вычисляем импульс
         impulse_scalar = -(1 + restitution) * velocity_along_normal
-        impulse_scalar /= (1 / self.player.mass + 1 / other.mass)
+        impulse_scalar /= (1 / ball.mass + 1 / other.mass)
 
         impulse = [impulse_scalar * normal[0], impulse_scalar * normal[1]]
 
         # Применяем импульс к шарам
-        self.player.velocity[0] += impulse[0] / self.player.mass
-        self.player.velocity[1] += impulse[1] / self.player.mass
+        ball.velocity[0] += impulse[0] / ball.mass
+        ball.velocity[1] += impulse[1] / ball.mass
         other.velocity[0] -= impulse[0] / other.mass
         other.velocity[1] -= impulse[1] / other.mass
 
     def check_collisions(self):
+
+
         for i in range(len(self.entities)):
+            entity1 = self.entities[i]
+
             for j in range(i+1, len(self.entities)):
-                other = self.entities[j]
-                dx = self.entities[i].pos[0] - other.pos[0]
-                dy = self.entities[i].pos[1] - other.pos[1]
+                entity2 = self.entities[j]
+                dx = entity1.pos[0] - entity2.pos[0]
+                dy = entity1.pos[1] - entity2.pos[1]
                 distance = math.sqrt(dx**2 + dy**2)
 
-        if distance < self.player.radius + other.radius:
-            self.resolve_collision(other)
-
+                if distance < entity1.radius + entity2.radius:
+                    self.resolve_collision(entity1, entity2)
+            
+            if entity1.pos[0] - entity1.radius < 0 or entity1.pos[0] + entity1.radius > 800:
+                entity1.velocity[0] = -entity1.velocity[0]
+                entity1.pos[0] = max(entity1.radius, min(screen.get_width() - entity1.radius, entity1.pos[0]))
+            if entity1.pos[1] - entity1.radius < 0 or entity1.pos[1] + entity1.radius > 600:
+                entity1.velocity[1] = -entity1.velocity[1]
+                entity1.pos[1] = max(entity1.radius, min(screen.get_height() - entity1.radius, entity1.pos[1]))
 
     def check_input(self):
         keys = pygame.key.get_pressed()
         player = self.entities[0]
+        print(player.velocity)
+
         # Управление красным шаром
         if keys[pygame.K_LEFT]:
-            self.player.velocity[0] -= SPEED
+            player.velocity[0] -= SPEED
         if keys[pygame.K_RIGHT]:
-            self.player.velocity[0] += SPEED
+            player.velocity[0] += SPEED
         if keys[pygame.K_UP]:
-            self.player.velocity[1] -= SPEED
+            player.velocity[1] -= SPEED
         if keys[pygame.K_DOWN]:
-            self.player.velocity[1] += SPEED
+            player.velocity[1] += SPEED
 
-        if self.player.velocity[0]!=0 or self.player.velocity[1]!=0:
-            length = math.sqrt(self.player.velocity[0]**2 + self.player.velocity[1]**2)
-            self.player.velocity[0]=(self.player.velocity[0]/length)*SPEED
-            self.player.velocity[1]=(self.player.velocity[1]/length)*SPEED
+
         
-        self.player.pos[0] += self.player.velocity[0]
-        self.player.pos[1] += self.player.velocity[1]
+        player.velocity[0] *= (1 - FRICTION)
+        player.velocity[1] *= (1 - FRICTION)
+        
+        player.pos[0] += player.velocity[0]
+        player.pos[1] += player.velocity[1]
 
 
-    def update(self,entities):
-        for entity in entities:
+    def update(self):
+        self.check_input()
+
+        for entity in self.entities:
+
+            entity.velocity[0] *= (1 - FRICTION)
+
             entity.pos[0] += entity.velocity[0]
             entity.pos[1] += entity.velocity[1]
+        
+        self.check_collisions()
+
 
 running=True
 enitites=[red_ball,blue_ball]
 game_state = GameState(enitites)
+
+# Главный игровой цикл
 while running:
 
     for event in pygame.event.get():
@@ -127,11 +151,6 @@ while running:
             running = False
     
     game_state.update()
-    
-    for i in range(len(game_state.entities)):
-        for j in range(i+1,len(game_state.entities)):
-            game_state.check_collision(game_state.entities[i],game_state.entities[j])
-    
    
 
     # Отрисовка
