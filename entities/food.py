7ddab8f4f72c868040d2213710a9da77dd30a24b
KOTO <olegkadushkin555@gmail.com>
2026-06-19 21:11:14 +0500

    добавил .is_alive и .is_eaten_by для Food

--- entities/food.py
+++ entities/food.py
@@ -4,9 +4,10 @@ class Food:
     def __init__(self,pos: tuple):
         self.pos = pos
         self.mass = 1
-        self.is_eaten = False
         self.team = None
         self.color = None
+        self.is_eaten_by = set()
+        self.is_alive=True
     @property
     def radius(self):
         return math.sqrt(self.mass)
