9e00b190ccd27dbf84c492eddc4d715a354e824c
KOTO <olegkadushkin555@gmail.com>
2026-06-21 23:57:25 +0500

    добавил share_food

--- ai/ai.py
+++ ai/ai.py
@@ -17,6 +17,9 @@ def calc_normal(pos1,pos2):
     else:
         return dist, (dx/dist,dy/dist)
 def calc_intent(ball:Ball,neighbour:Ball,more_follow,more_unfollow):
+    if ball.team!=config.TEAM_BLUE:
+        more_follow=0
+        more_unfollow=0
     if neighbour.radius==ball.radius:
         return 0
     danger_factor = 0
@@ -33,15 +36,16 @@ def calc_intent(ball:Ball,neighbour:Ball,more_follow,more_unfollow):
 def calc_importance_factor(dist):
     if dist==0:
         return 0
-    return config.FOOD_MASS/(1+dist)+config.IMPORTANCE_KOEF
+    return config.FOOD_MASS/(0.1*dist)+config.IMPORTANCE_KOEF
 def ai(ball:Ball.Ball,neighbours:list):
     mouse_keys = pygame.mouse.get_pressed()
     mouse_pos = pygame.mouse.get_pos()
-    
+    keyboard_keys = pygame.key.get_pressed()
+    space = keyboard_keys[pygame.K_SPACE]
     more_follow = mouse_keys[0]
     more_unfollow = 2*mouse_keys[2]
     more_ignore = mouse_keys[1]
-
+    ball.sharing_food_factor = space
 
     #reflex_intent (begin)
     total_reflex_intent = intent.Intent([0,0],config.COLOR_BLACK)
@@ -94,11 +98,11 @@ def ai(ball:Ball.Ball,neighbours:list):
 
     max_intent = max(intents, key=lambda b: math.hypot(b.pos[0], b.pos[1]))    
     max_intent_length = math.hypot(max_intent.pos[0],max_intent.pos[1])
-    command_factor = (1 + more_follow - more_unfollow - more_ignore)*max_intent_length
+    command_factor = (1 + more_follow - more_unfollow - more_ignore)*(max_intent_length+1)
     command_intent = intent.Intent([0,0],config.COLOR_ORANGE)
     if ball.team==config.TEAM_BLUE:
         command_dist,command_normal = calc_normal(ball.pos,mouse_pos)
-        command_intent.pos[0] += command_normal[0] * command_factor * calc_importance_factor(command_dist)
+        command_intent.pos[0] += command_normal[0] * command_factor *calc_importance_factor(command_dist)
         command_intent.pos[1] += command_normal[1]*command_factor*calc_importance_factor(command_dist)
     #command_intent (end)
     intents.append(command_intent)
